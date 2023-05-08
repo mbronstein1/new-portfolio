80de8f3934aa76f255313f98778e493175449b0f
Matthew Bronstein <mbronstein1@gmail.com>
2023-05-08 14:46:48 -0500

    still troubleshooting gh-pages error

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import Portfolio from './pages/Portfolio';
 import Home from './pages/Home';
 import Layout from './components/UI/Layout';
 
-function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <>
-        <Route path='' element={<Layout />}>
-          <Route index={true} element={<Home />} />
-          <Route path='portfolio' element={<Portfolio />} />
-        </Route>
-      </>
-    )
-  );
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path='/' element={<Layout />}>
+        <Route index={true} element={<Home />} />
+        <Route path='/portfolio' element={<Portfolio />} />
+      </Route>
+    </>
+  )
+);
 
+function App() {
   return <RouterProvider router={router} />;
 }
 
