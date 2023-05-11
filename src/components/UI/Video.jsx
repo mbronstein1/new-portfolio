56dccfdd44faa009f4b4418a0a6f1861079fe674
Matthew Bronstein <mbronstein1@gmail.com>
2023-05-11 10:08:14 -0500

    experimenting with video on movile

--- src/components/UI/Video.jsx
+++ src/components/UI/Video.jsx
@@ -5,8 +5,8 @@ import classes from './Video.module.css';
 // Background video
 const Video = () => {
   return (
-    <video className={classes.video} playsInline autoPlay muted loop>
-      <source src={video} type='video/webm' />
+    <video className={classes.video} playsinline autoPlay muted loop>
+      <source src={video} type='video/mp4' />
     </video>
   );
 };
