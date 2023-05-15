7205015b1f6913e0fb1ca571ababbfa94cb6b646
Matthew Bronstein <mbronstein1@gmail.com>
2023-05-15 10:30:14 -0500

    changed header transition on mobile drawer close from transform to opacity

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,11 +9,10 @@ const NAV_ITEMS = ['About', 'Portfolio', 'Contact'];
 const Navbar = ({ isNavBarTop, isMobile, isOpen, setIsOpen }) => {
   return (
     <nav className={`${classes.navbar} ${isNavBarTop}`}>
-      {
-        <HashLink to='/#top' smooth scroll={scrollWithOffset} style={{ transform: isOpen ? 'translateX(-100px)' : 'translateX(0px)', transition: '.5s all ease-in-out' }}>
-          <h1>Matthew Bronstein</h1>
-        </HashLink>
-      }
+      <HashLink to='/#top' smooth scroll={scrollWithOffset} style={{ opacity: isOpen ? 0 : 1, transition: '.5s all' }}>
+        <h1>Matthew Bronstein</h1>
+      </HashLink>
+
       {isMobile ? (
         <Hamburger toggled={isOpen} toggle={setIsOpen} />
       ) : (
